a9fb3abf323fce0d3e519d8fb64628a934543d9a
Nimisha <goel.nimishaa@gmail.com>
2019-08-20 21:41:04 -0400

    authentication tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name : {
         type : String,
         required :true,
@@ -20,6 +22,7 @@ const User = mongoose.model('User', {
     },
     email :{
         type : String,
+        unique : true,
         trim: true,
         lowercase : true,
         validate(value){
@@ -32,7 +35,54 @@ const User = mongoose.model('User', {
         type : Number,
         default : 0
 
-    }
+    },
+    tokens: [{
+        token : {
+            type : String,
+            required : true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function (){
+    const user = this
+
+    const token = jwt.sign({_id : user._id.toString()}, "thisisatokenchapter")
+
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+
+}
+
+userSchema.statics.findByCredentials = async (email,password) =>{
+    const user = await User.findOne({email})
+
+    if(!user)
+    {
+        throw new Error("Unable to login")
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch)
+    {
+        throw new Error("Unable to login")
+    }
+    return user
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    
+    if(user.isModified('password'))
+    {
+        user.password = await bcrypt.hash(user.password,8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema )
 
 module.exports = User
